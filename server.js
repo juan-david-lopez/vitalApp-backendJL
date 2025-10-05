00cd75e132a47bec41cd9a61f45be73582f7adc6
JuanDavidLopezH <juand.lopezh@uqvirtual.edu.co>
2025-10-05 18:29:03 -0500

    fix: problemas con eslint - disable linebreak-style and remove unused next parameter

--- server.js
+++ server.js
@@ -276,7 +276,7 @@ app.use((req, res) => {
   });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.stack);
   res.status(500).json({
     success: false,
